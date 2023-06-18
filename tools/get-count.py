b2abce7271e90e2edf8cf63246d55b8a2ce50787
klappscheinwerfer <toms.grants@gmail.com>
2023-06-18 15:08:48 +0300

    DT

--- tools/get-count.py
+++ tools/get-count.py
@@ -1,18 +1,27 @@
 # Counts how many questions already have an answer
+# Option -s shows questions with no answers
 
 import json
+import sys
 
-f = open("data/progr_tehn_2023_CPKE.json")
-data = json.load(f)
 
-answers = 0
-questions = 0
+def get_count(show_no_answer):
+	f = open("data/progr_tehn_2023_CPKE.json")
+	data = json.load(f)
 
-for i in data:
-	questions += 1
-	if i["correct"] != None:
-		answers += 1
+	answers = 0
+	questions = 0
 
-print("{} / {}".format(answers, questions))
+	for i in data:
+		questions += 1
+		if i["correct"] != None:
+			answers += 1
+		elif show_no_answer:
+			print(i["question"])
 
-f.close()
+	print("{} / {}".format(answers, questions))
+
+	f.close()
+
+if __name__ == '__main__':
+	get_count('-s' in sys.argv)
