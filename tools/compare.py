8e2c7914070e06c045d83fba50ca7a52fc8736c8
klappscheinwerfer <toms.grants@gmail.com>
2025-06-20 23:43:41 +0300

    Add ability to find questions with different IDs and correct answers

--- tools/compare.py
+++ tools/compare.py
@@ -27,9 +27,27 @@ args = parser.parse_args()
 if len(args.files) < 2:
 	parser.error("At least two file paths are required.")
 
-# Load all files
-questions = []
+# Load all files, find differences
+questions = {} # Dict, questions are grouped by their text
+q_diff_ids = [] # Questions with different IDs
+q_diff_text = [] # Questions with different text
+q_diff_cansw = [] # Questions with different correct answer
 for filepath in args.files:
 	with open(filepath, "r") as json_file:
-		questions.extend(x for x in json.load(json_file) if x not in questions)
-print(questions)
+		for x in json.load(json_file):
+			key = x["question"]
+			if key in questions:
+				if questions[key] != [x]:
+					questions[key].append(x)
+					if (questions[key][0]["id"] != x["id"]):
+						q_diff_ids.append(f"{questions[key][0]["id"]} {x["id"]}")
+					if (questions[key][0]["correct"] != x["correct"]):
+						q_diff_cansw.append(f"{questions[key][0]["id"]} {questions[key][0]["correct"]} | {x["id"]} {x["correct"]}")
+			else:
+				questions[key] = [x]
+
+# Print results
+# for q in q_diff_ids:
+#	print(q)
+for q in q_diff_cansw:
+	print(q)
