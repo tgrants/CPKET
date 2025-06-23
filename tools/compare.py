4f7084b7438cd2912c8ee34c33544bd84d96aea2
klappscheinwerfer <toms.grants@gmail.com>
2025-06-23 16:09:03 +0300

    Change question data structure, add comparison arguments

--- tools/compare.py
+++ tools/compare.py
@@ -4,14 +4,31 @@ This script compares answers to the same questions across multiple tests.
 It is useful for finding and eliminating inconsistencies.
 
 (c) Toms Grants, MIT License
-https://github.com/tgrants/CET
+https://github.com/tgrants/CPKET
 
 Command-line Arguments:
 	-h, --help: Displays a list of all commands
+	-c, --compare-correct: Compares correct answers for questions with the same text
+	-i, --compare-ids: Compares IDs for questions with the same text
 """
 
 import json
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from enum import Enum
+from pathlib import Path
+
+
+class DiffType(Enum):
+	ID = "id"
+	CORRECT = "correct"
+
+
+def hyperlink(label: str, path: str, line: int = None) -> str:
+	if line:
+		url = f"file://{path}:{line}"
+	else:
+		url = f"file://{path}"
+	return f"\033]8;;{url}\a{label}\033]8;;\a"
 
 
 # Parse and validate arguments
@@ -23,31 +40,48 @@ parser.add_argument(
 	nargs = "+",
 	help = "Paths to the input files",
 )
+me_group = parser.add_mutually_exclusive_group(required = False)
+me_group.add_argument(
+	'-c', '--compare-correct',
+	action='store_true',
+	help = "Compare correct answers for questions with the same text",
+)
+me_group.add_argument(
+	'-i', '--compare-ids',
+	action='store_true',
+	help = "Compare IDs for questions with the same text",
+)
 args = parser.parse_args()
 if len(args.files) < 2:
 	parser.error("At least two file paths are required.")
+# If no comparison flags are passed, choose a default
+if not any([
+	args.compare_correct,
+	args.compare_ids,
+]):
+	print("No comparison flags specified. Defaulting to correct answer comparison.")
+	args.compare_correct = True
 
 # Load all files, find differences
 questions = {} # Dict, questions are grouped by their text
-q_diff_ids = [] # Questions with different IDs
-q_diff_text = [] # Questions with different text
-q_diff_cansw = [] # Questions with different correct answer
 for filepath in args.files:
 	with open(filepath, "r") as json_file:
 		for x in json.load(json_file):
 			key = x["question"]
 			if key in questions:
-				if questions[key] != [x]:
-					questions[key].append(x)
-					if (questions[key][0]["id"] != x["id"]):
-						q_diff_ids.append(f"{questions[key][0]["id"]} {x["id"]}")
-					if (questions[key][0]["correct"] != x["correct"]):
-						q_diff_cansw.append(f"{questions[key][0]["id"]} {questions[key][0]["correct"]} | {x["id"]} {x["correct"]}")
+				existing_question = questions[key][0][1]
+				if existing_question != x:
+					diff = None # Difference type for alt question
+					if (args.compare_ids and existing_question["id"] != x["id"]):
+						diff = DiffType.ID
+					elif (args.compare_correct and existing_question["correct"] != x["correct"]):
+						diff = DiffType.CORRECT
+					questions[key].append((filepath, x, diff))
 			else:
-				questions[key] = [x]
+				questions[key] = [(filepath, x, None)]
 
 # Print results
-# for q in q_diff_ids:
-#	print(q)
-for q in q_diff_cansw:
-	print(q)
+for key, value in questions.items():
+	if len(value) <= 1: continue # Skip questions without differences
+	print(f"Q '{str(key).replace("\n", " ")}'")
+	print()
